ed48a069446747b8e9a3a074dd94007da48dba4e
Pranali0502 <pranaliragh@gmail.com>
2022-11-29 23:06:32 +0530

    c

--- components/connect.php
+++ components/connect.php
@@ -4,7 +4,15 @@ $db_name = 'mysql:host=localhost;dbname=shop_db';
 $user_name = 'root';
 $user_password = '';
 
-$conn = new PDO($db_name, $user_name , $user_password);
+// $host = 'remotemysql.com';
+// $db_name = 'GBvR8CEHSQ';
+// $user_name = 'GBvR8CEHSQ';
+// $user_password = 'veSO5LGyZ2';
+// $dsn = 'mysql:host=$host;dbname= $db_name';
+
+$conn = new PDO( $db_name, $user_name , $user_password);
+// $conn = new PDO($dsn, $user_name , $user_password);
 $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING ); 
 $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION ); 
 ?>
+
--- shop_db.sql
+++ shop_db.sql
@@ -3,8 +3,6 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 30, 2022 at 12:51 PM
--- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
